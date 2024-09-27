371273ba833275ee211c887e1c462c4d2d0fac74
lionelalfonso0 <lionelalfonso0@gmail.com>
2024-09-27 17:09:19 -0300

    Listado agentes HTML y JS

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*async function mostrarAgentes(){
+async function mostrarAgentes(){
 const info = document.querySelector(".valorant");
 
 console.log(info);
@@ -11,23 +11,14 @@ const agentes= await fetch(`https://valorant-api.com/v1/agents`)
     agentes.map(agente => {
         if(agente.isPlayableCharacter){
             console.log(agente);
-            const div = document.createElement("div")
-            const titulo= document.createElement("h2")
-            titulo.textContent = agente.displayName;
-            const img = document.createElement("img");
-            img.src = agente.displayIcon;
-            div.append(titulo, img);
+            console.log(agente.uuid);
+            console.log(agente.displayName);  
             
-            agente.abilities.map(habilidad =>{
-                const habilidadDiv = document.createElement("div")
-                const habilidadP = document.createElement("p");
-                habilidadP.textContent = habilidad.displayName;
-                habilidadDiv.appendChild(habilidadP);
-                div.appendChild(habilidadDiv);
-            })
-            info.appendChild(div);
+            /*agente.abilities.map(habilidad =>{
+                console.log(habilidad);
+            })*/
         }
     });
 }
+mostrarAgentes();
 
-mostrarAgentes();*/
